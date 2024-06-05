8fe7f123bbeb26ee03d9940d56bffdde4c5f67b8
Pond <pondwachi.th@gmail.com>
2024-06-05 12:40:22 +0700

    feat: react state of pond

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [sayHi, setSayHi] = useState('Greeting Message');
+
+  const greetingChange = (message) => {
+    setSayHi(message)
+  }
+ 
+
+  
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container" >{sayHi}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => greetingChange('สวัสดี!')}>สวัสดี!</button>
+        <button onClick={() => greetingChange('Hi!')} >Hi!</button>
+        <button onClick={() => greetingChange('你好!')} >你好!</button>
       </div>
     </div>
   );
